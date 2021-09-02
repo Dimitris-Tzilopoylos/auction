4ec65bf01f014a374bb06979097464dbab9050f1
Dimitris-Tzilopoylos <59147831+Dimitris-Tzilopoylos@users.noreply.github.com>
2021-09-03 01:13:50 +0300

    Update login.js

--- router/core/login.js
+++ router/core/login.js
@@ -3,7 +3,7 @@ const express = require('express')
 const router = express.Router()
 
 const authControllers = require('../../controllers/auth')
-
+ 
 router.get('/login',authControllers.getLogin)
 router.get('/register',authControllers.getRegister)
 router.post('/login',authControllers.postLogin)
